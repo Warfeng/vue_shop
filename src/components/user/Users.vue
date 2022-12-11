764158b2eb17c4c98aab9edeae4e21f858f783ba
Warfeng <1090645743@qq.com>
2022-12-11 16:39:06 +0800

    用户列表功能的开发

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -94,8 +94,6 @@
 </template>
 
 <script>
-import { info } from 'console'
-
 export default {
   data() {
     var checkEmail = (rule, value, cb) => {
@@ -243,18 +241,23 @@ export default {
       })
     },
     // 删除用户
-    removeUserById(id) {
-      // const confirmResult = await this.$confirm('此操作将会永久删除该用户，是否继续？', '提示', {
-      //   confirmButtonText: '确定',
-      //   cancelButtonText: '取消',
-      //   type: 'warning'
-      // }).catch(err => err)
+    async removeUserById(id) {
+      const confirmResult = await this.$confirm('此操作将会永久删除该用户，是否继续？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
 
-      // if (confirmResult !== 'confirm') {
-      //   return this.$message.error('已取消删除')
-      // }
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('已取消删除')
+      }
 
-      console.log('确认已删除')
+      const { data: res } = await this.$http.delete(`users/${id}`)
+      if(res.meta.status !== 200) {
+        this.$message.error('删除用户失败')
+      }
+      this.$message.success('删除用户成功！')
+      this.getUserList()
     }
   }
 }
